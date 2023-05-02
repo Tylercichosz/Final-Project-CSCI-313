b5b6aaf86182f81179d7b92506b722eb180a4a16
Tylercichosz <Tyler.Cichosz@ndus.edu>
2023-05-02 13:33:40 -0500

    commit

--- final_project/first_page/models.py
+++ final_project/first_page/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 
+
 class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    duration = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.title
-
+    
--- final_project/first_page/views.py
+++ final_project/first_page/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect 
 from .models import Course
-
+from .forms import CourseForm
 
 
 def first_homePage(request):
@@ -9,16 +9,6 @@ def about_basePage(request):
     return render(request, 'about_Us.html')
 def profile_basePage(request):
     return render(request, 'profile.html')
-
-def courses(request):
-    courses = [
-        {'title': 'Course 1', 'description': 'Description of Course 1'},
-        {'title': 'Course 2', 'description': 'Description of Course 2'},
-        {'title': 'Course 3', 'description': 'Description of Course 3'}
-
-
-    ]
-    return render(request,'courses.html', {'courses': courses})\
         
 def course_list(request):
     courses = Course.objects.all()
@@ -27,3 +17,18 @@ def course_list(request):
 def course_detail(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     return render(request, 'course_detail.html', {'course': course})
+
+def create_course(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')
+    else:
+        form = CourseForm()
+    return render(request, 'create_course.html', {'form': form})
+
+def course_list(request):
+    courses = Course.objects.all()
+    form = CourseForm()
+    return render(request, 'courses.html', {'courses': courses, 'form': form})
