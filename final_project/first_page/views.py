a4dbde33a6b17ada691ba7d1e8a828f44d43f147
Tylercichosz <Tyler.Cichosz@ndus.edu>
2023-05-09 14:02:58 -0500

    commit

--- final_project/first_page/views.py
+++ final_project/first_page/views.py
@@ -16,38 +16,6 @@ def courses(request):
     courses = Course.objects.all()
     return render(request, 'courses.html', {'courses': courses})
 
-def course_list(request):
-    courses = Course.objects.all()
-
-    if request.method == 'POST':
-        form = CourseForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('course_list')
-    else:
-        form = CourseForm()
-
-    context = {
-        'courses': courses,
-        'form': form
-    }
-    return render(request, 'course_list.html', context)
-
-def course_detail(request, course_id):
-    course = get_object_or_404(Course, pk=course_id)
-    context = {'course': course}
-    return render(request, 'course_detail.html', context)
-
-def create_course(request):
-    if request.method == 'POST':
-        form = CourseForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('course_list')
-    else:
-        form = CourseForm()
-    return render(request, 'create_course.html', {'form': form})
-
 def course_add(request):
     if request.method == 'POST':
         form = CourseForm(request.POST)
