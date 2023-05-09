cdbd6fa0c998b2fca84336fa50384ad98fca7355
Tylercichosz <Tyler.Cichosz@ndus.edu>
2023-05-09 15:12:48 -0500

    commit

--- final_project/first_page/models.py
+++ final_project/first_page/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    totalclasses = models.IntegerField(default=0, null=True, blank=True)
+    classesattended = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.title
